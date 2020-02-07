11f3dfe13c5ffd5566e2767e9134e75f1303141c
Ridisha Shetty <ridishashetty@gmail.com>
2020-02-07 14:35:00 -0600

    mePage

--- src/app/me/me.page.ts
+++ src/app/me/me.page.ts
@@ -1,15 +1,33 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import {ConstantsService} from '../constants.service';
 
 @Component({
   selector: 'app-me',
   templateUrl: './me.page.html',
   styleUrls: ['./me.page.scss'],
 })
-export class MePage implements OnInit {
+export class MePage implements OnInit{
 
-  constructor() { }
+  username: string;
+
+  constructor(private router: Router,
+              private constant: ConstantsService) { }
 
   ngOnInit() {
+
+    this.username = this.constant.username;
+    console.log(this.username);
+  }
+  login() {
+    this.router.navigate(['tabs/home']);
   }
 
+  myorder() {
+    this.router.navigate(['tabs/order']);
+  }
+
+  personalInfo() {
+    this.router.navigate(['tabs/home']);
+  }
 }
